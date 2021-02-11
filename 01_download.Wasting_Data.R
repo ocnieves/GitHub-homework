39519432f2112fc53276be39d9dc787432b81ac9
ocnieves <ocnieves@bu.edu>
2021-02-10 19:09:03 -0500

    Changed read_csv tp read.csv

--- 01_download.Wasting_Data.R
+++ 01_download.Wasting_Data.R
@@ -1,4 +1,3 @@
-{r, eval=FALSE}
 install.packages("readr")
 library(readr)
-data <- read_csv("https://raw.githubusercontent.com/daviessw/BI586/master/R_Worksheet/Wasting_Data.csv", skip=1)
+data <- read.csv("https://raw.githubusercontent.com/daviessw/BI586/master/R_Worksheet/Wasting_Data.csv", skip=1)
